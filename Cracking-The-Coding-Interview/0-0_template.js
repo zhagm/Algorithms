77d01a9a98d593c3c89c36c124cc1220141f31b1
Zhaganbike <zhagdev@gmail.com>
2017-07-05 12:51:47 -0400

    typed up code, added template file for future

---
+++ Cracking-The-Coding-Interview/0-0_template.js
@@ -0,0 +1,17 @@
+"use strict";
+// to allow use of let instead of var (ES6)
+
+// Instructions:
+// ...
+
+function func(x) {
+  return x;
+}
+
+console.log("func('test'):", func('test'));
+// answer
+
+/*
+Pseudocode:
+//
+*/
---
+++ Cracking-The-Coding-Interview/1-2_checkPermutation.js
@@ -0,0 +1,38 @@
+"use strict";
+// to allow use of let instead of var (ES6)
+
+// Instructions:
+// Take input of 2 strs, check if one is a permutation of the other
+
+function checkPermutation(s1, s2) {
+  if (s1.length !== s2.length) return false;
+
+  const dict = {};
+
+  for (let i = 0; i < s1.length; i++) {
+    const char = s1[i];
+    dict[char] = dict[char] ? dict[char] + 1 : 1;
+  }
+  
+  for (let j = 0; j < s2.length; j++) {
+    const char = s1[j];
+    if (dict[char]) {
+      dict[char]--;
+    }
+    else return false;
+  }
+
+  return true;
+}
+
+// console.log("isUnique('hello'):", isUnique('hello'));
+// // false
+
+/*
+Pseudocode:
+// easy first check s1 and s2 lengths should be the same, if not return false
+// create dictionary object, loop through s1 and fill dictionary
+// with char: count key: value pairs
+// loop through s2, if dictionary[char] doesnt exist or is below 0, return false
+// else subtract 1 count, if reaches end of function return true
+*/
