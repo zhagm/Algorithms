f34e1690afd38a900939abbce46b68e343931cb8
Zhaganbike <zhagdev@gmail.com>
2017-07-09 21:02:56 -0400

    init 1-5_oneAway, pseudo code, instructions, and tests

---
+++ Cracking-The-Coding-Interview/1-5_oneAway.js
@@ -0,0 +1,33 @@
+"use strict";
+// to allow use of let/const instead of var (ES6)
+
+// Instructions:
+// There are three types of edits that can be performed on strings: insert a
+// character, remove a character, or replace a character. Given two strings,
+// write a function to check if they are one edit (or zero edits) away.
+
+function oneAway(str1, str2) {
+  return str1;
+}
+
+console.log(oneAway('ple', 'pale'));
+// true
+
+console.log(oneAway('pales', 'pale'));
+// true
+
+console.log(oneAway('pale', 'bale'));
+// true
+
+console.log(oneAway('pale', 'bake'));
+// false
+
+
+/*
+Pseudocode:
+// First check if they are the same string to save trouble
+// If they are the same length, loop through and count differences
+// If differences are 1 count, return true, else false
+// If they're one away from each other in length, loop through, knowing longer
+// and shorter one, and try taking out one char at a time to see if they match
+*/
