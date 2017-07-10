5b87f8488d3da5ecf0821ed6eb15fced9de0ddac
Zhaganbike <zhagdev@gmail.com>
2017-07-10 09:44:48 -0400

    1-6 init, instructions and test

---
+++ Cracking-The-Coding-Interview/1-6_stringCompression.js
@@ -0,0 +1,21 @@
+// Instructions:
+// Implement a method to perform basic string compression using the counts of
+// repeated characters. For example, the string aabcccccaaa would become
+// a2b1c5a3. If the "compressed" string would not become smaller than the
+// original string, your method should return the original string. You can
+// assume the string has only uppercase and lowercase letters (a - z).
+
+function stringCompression(x) {
+  return x;
+}
+
+console.log("stringCompression('aabcccccaaa'):", stringCompression('aabcccccaaa'));
+// a2b1c5a3
+
+console.log("stringCompression('abcba'):", stringCompression('abcba'));
+// a1b1c1b1a1
+
+/*
+Pseudocode:
+//
+*/
