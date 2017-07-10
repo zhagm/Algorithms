7f16a7ea96113c2187181b85ed844f12646434db
Zhaganbike <zhagdev@gmail.com>
2017-07-10 09:48:12 -0400

    1-6 completed

--- Cracking-The-Coding-Interview/1-6_stringCompression.js
+++ Cracking-The-Coding-Interview/1-6_stringCompression.js
@@ -5,17 +5,33 @@
 // original string, your method should return the original string. You can
 // assume the string has only uppercase and lowercase letters (a - z).
 
-function stringCompression(x) {
-  return x;
+function stringCompression(str) {
+  let newStr = '';
+  let curr, count;
+  for ( let i = 0; i < str.length; i++ ) {
+    if (!curr) {
+      curr = str[i];
+      count = 1;
+    }
+    if (str[i] === curr && str[i + 1] !== curr) {
+      newStr += curr + count;
+      curr = '', count = 0;
+    } else {
+      count++;
+    }
+  }
+  return newStr;
 }
 
 console.log("stringCompression('aabcccccaaa'):", stringCompression('aabcccccaaa'));
 // a2b1c5a3
 
-console.log("stringCompression('abcba'):", stringCompression('abcba'));
-// a1b1c1b1a1
+console.log("stringCompression('ab*****ba'):", stringCompression('ab*****ba'));
+// a1b1*5b1a1
 
 /*
 Pseudocode:
-//
+// create result string empty. create current and count vars to keep track
+// loop through string, evaluate current and count, and next char str[i+1]
+// to determine if to concat to result string. return result string at end
 */
