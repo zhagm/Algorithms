5b3d54c9681d8d26ce913bea68350ef5b108c7e0
Zhaganbike <zhagdev@gmail.com>
2017-07-11 10:22:18 -0400

    1-9 solution

--- Cracking-The-Coding-Interview/1-9_stringRotation.js
+++ Cracking-The-Coding-Interview/1-9_stringRotation.js
@@ -2,13 +2,18 @@
 // Given 2 strings, check if s1 is a rotation of s2
 
 function stringRotation(s1, s2) {
+  if (s1.length !== s2.length) return false;
+  for ( let i = 0; i < s1.length; i++ ) {
+    if (s2 === s1.substr(i, s1.length - 1) + s1.substr(0, i))
+      return true;
+  }
   return false;
 }
 
-console.log("stringRotation('hello, ehllo'):", stringRotation('hello, ehllo'));
+console.log("stringRotation('hello', 'ehllo'):", stringRotation('hello', 'ehllo'));
 // false
 
-console.log("stringRotation('waterbottle, erbottlewat'):", stringRotation('waterbottle, erbottlewat'));
+console.log("stringRotation('waterbottle', 'erbottlewat'):", stringRotation('waterbottle', 'erbottlewat'));
 // true
 
 /*
