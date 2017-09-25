9150dc418a145ce3446ab6265ed54085b0cfb437
zhagm <zhagdev@gmail.com>
2017-09-25 13:40:20 +0700

    new dir for general practice, uncategorized, singly linked list practice

---
+++ Cracking-The-Coding-Interview/2-1_removeDupesINCOMPLETE.js
@@ -0,0 +1,16 @@
+// Instructions:
+// Write code to remove duplicates from an unsorted linked list.
+// FOLLOW UP:
+// How would you solve this problem if a temporary buffer is not allowed?
+
+function removeDupes(linkedList) {
+  return x;
+}
+
+console.log("removeDupes('test'):", removeDupes('test'));
+// answer
+
+/*
+Pseudocode:
+//
+*/
