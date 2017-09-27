96d06b0ea0cc867c224fde5c4be08e1f398fcd21
zhagm <zhagdev@gmail.com>
2017-09-27 13:21:18 +0700

    init 2-3, updated streak

---
+++ Cracking-The-Coding-Interview/2-3_deleteMiddleNode.js
@@ -0,0 +1,16 @@
+// Instructions:
+// Implement an algorithm to delete a node in the middle (i.e., any node but the
+// first and last node, not necessarily the exact middle) of a singly list, given
+// only access to that node.
+
+function deleteMiddleNode(x) {
+  return;
+}
+
+console.log("deleteMiddleNode(test1):", deleteMiddleNode(test1), test1);
+// length should be 4, without the 2nd node
+
+/*
+Pseudocode:
+//
+*/
