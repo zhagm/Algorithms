641c7f084e9c9c682fea797ad5cfb94b9627b8ef
zhagm <zhagdev@gmail.com>
2020-01-11 09:59:03 -0800

    Add someRecursive and flatten

--- JS-Algorithms&DataStructs/08_Recursion/08_someRecursive.js
+++ JS-Algorithms&DataStructs/08_Recursion/08_someRecursive.js
@@ -3,12 +3,33 @@
 // The function returns true if a single value in the array returns true when passed to the callback.
 // Otherwise it returns false.
 
-function someRecursive(x) {
-  return x;
+function someRecursive(arr, cb) {
+  if (!arr.length) return false;
+  return cb(arr[0]) || someRecursive(arr.slice(1), cb);
 }
 
-console.log("someRecursive('test'):", someRecursive("test"));
-// answer
+const isOdd = val => val % 2 !== 0;
+
+console.log(
+  "someRecursive([1,2,3,4], isOdd):",
+  someRecursive([1, 2, 3, 4], isOdd)
+);
+// true
+
+console.log(
+  "someRecursive([4,6,8,9], isOdd):",
+  someRecursive([4, 6, 8, 9], isOdd)
+);
+// true
+
+console.log("someRecursive([4,6,8], isOdd):", someRecursive([4, 6, 8], isOdd));
+// false
+
+console.log(
+  "someRecursive([4,6,8], val => val > 10):",
+  someRecursive([4, 6, 8], isOdd)
+);
+// false
 
 /*
 Pseudocode:
--- JS-Algorithms&DataStructs/08_Recursion/09_flatten.js
+++ JS-Algorithms&DataStructs/08_Recursion/09_flatten.js
@@ -1,12 +1,22 @@
 // Instructions:
-// ...
+// Write a recursive function called flatten which accepts an array of arrays
+// and return s a new array with all values flattened.
 
-function func(x) {
-  return x;
+function flatten(arr) {
+  if (!arr.length) return [];
 }
 
-console.log("func('test'):", func('test'));
-// answer
+console.log("flatten([1, 2, 3, [4, 5] ]):", flatten([1, 2, 3, [4, 5]]));
+// [1, 2, 3, 4, 5]
+
+console.log(
+  "flatten([1, [2, [3, 4], [[5]]]]):",
+  flatten([1, [2, [3, 4], [[5]]]])
+);
+// [1, 2, 3, 4, 5]
+
+console.log("flatten([[1],[2],[3]]):", flatten([[1], [2], [3]]));
+// [1,2,3]
 
 /*
 Pseudocode:
