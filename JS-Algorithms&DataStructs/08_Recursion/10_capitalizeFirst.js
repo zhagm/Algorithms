e41cd276eab29ee5ff08cb4c8393a03f85b10473
zhagm <zhagdev@gmail.com>
2020-01-13 08:53:26 -0800

    Complete capitalizeFirst recursive function

--- JS-Algorithms&DataStructs/08_Recursion/10_capitalizeFirst.js
+++ JS-Algorithms&DataStructs/08_Recursion/10_capitalizeFirst.js
@@ -2,11 +2,17 @@
 // Write a recursive function called capitalizeFirst.
 // Given an array of strings, capitalize the first letter of each string in the array.
 
-function capitalizeFirst(x) {
-  return x;
+function capitalizeFirst(arr) {
+  if (arr.length === 0) return [];
+  let curr = arr.shift();
+  curr = curr[0].toUpperCase() + curr.slice(1);
+  return [curr, ...capitalizeFirst(arr)];
 }
 
-console.log("capitalizeFirst('test'):", capitalizeFirst("test"));
+console.log(
+  "capitalizeFirst(['hello', 'how', 'are', 'you']):",
+  capitalizeFirst(["hello", "how", "are", "you"])
+);
 // answer
 
 /*
