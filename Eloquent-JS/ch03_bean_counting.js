e7019014948c296fc645b3be77d001b85ccb9ec6
zhagm <zhagdev@gmail.com>
2020-01-16 14:48:46 -0800

    Complete ch03_bean_counting from Eloquent JS

--- Eloquent-JS/ch03_bean_counting.js
+++ Eloquent-JS/ch03_bean_counting.js
@@ -1,13 +1,28 @@
 /*
 Bean counting
-You can get the Nth character, or letter, from a string by writing "string"[N]. The returned value will be a string containing only one character (for example, "b"). The first character has position 0, which causes the last one to be found at position string.length - 1. In other words, a two-character string has length 2, and its characters have positions 0 and 1.
+You can get the Nth character, or letter, from a string by writing "string"[N]. 
+The returned value will be a string containing only one character (for example, "b"). 
+The first character has position 0, which causes the last one to be found at position string.length - 1. 
+In other words, a two-character string has length 2, and its characters have positions 0 and 1.
 
-Write a function countBs that takes a string as its only argument and returns a number that indicates how many uppercase “B” characters there are in the string.
+Write a function countBs that takes a string as its only argument and returns a number that 
+indicates how many uppercase “B” characters there are in the string.
 
-Next, write a function called countChar that behaves like countBs, except it takes a second argument that indicates the character that is to be counted (rather than counting only uppercase “B” characters). Rewrite countBs to make use of this new function.
+Next, write a function called countChar that behaves like countBs, except it takes a second argument 
+that indicates the character that is to be counted (rather than counting only uppercase “B” characters). Rewrite countBs to make use of this new function.
 */
 
-// Your code here.
+function countBs(str) {
+  let bCount = 0;
+  for (let letter of str) bCount += letter === "B";
+  return bCount;
+}
+
+function countChar(str, char) {
+  let count = 0;
+  for (let letter of str) count += letter === char;
+  return count;
+}
 
 console.log(countBs("BBC"));
 // → 2
