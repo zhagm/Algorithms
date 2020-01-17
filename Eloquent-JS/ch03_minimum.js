b3dfff6311804b4878ff5a41ab2746e89ad3e1aa
zhagm <zhagdev@gmail.com>
2020-01-17 14:34:22 -0800

    Complete more recursive functions in Eloquent

--- Eloquent-JS/ch03_minimum.js
+++ Eloquent-JS/ch03_minimum.js
@@ -1,8 +1,10 @@
 /* Minimum
-The previous chapter introduced the standard function Math.min that returns its smallest argument. We can build something like that now. Write a function min that takes two arguments and returns their minimum.
+The previous chapter introduced the standard function Math.min that returns its smallest argument. 
+We can build something like that now. Write a function min that takes two arguments and returns their minimum.
 */
 
 function min(a, b) {
+  return a < b ? a : b;
 }
 
 console.log(min(0, 10));
--- Eloquent-JS/ch03_recursion.js
+++ Eloquent-JS/ch03_recursion.js
@@ -12,7 +12,12 @@ Define a recursive function isEven corresponding to this description. The functi
 Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a way to fix this?
 */
 
-// Your code here.
+function isEven(n) {
+  if (n < 0) n = Math.abs(n);
+  if (n === 0) return true;
+  else if (n === 1) return false;
+  return isEven(n - 2);
+}
 
 console.log(isEven(50));
 // → true
