faa8ed0c8dc4b367a1f38ad76b6a766063fd2b83
zhagm <zhagdev@gmail.com>
2020-01-26 09:51:24 -0800

    Start working on Robot project

--- Eloquent-JS/ch07_robot.js
+++ Eloquent-JS/ch07_robot.js
@@ -32,3 +32,36 @@ function buildGraph(edges) {
 }
 const roadGraph = buildGraph(roads);
 console.log(roadGraph);
+
+class VillageState {
+  constructor(place, parcels) {
+    this.place = place;
+    this.parcels = parcels;
+  }
+
+  move(destination) {
+    if (!roadGraph[this.place].includes(destination)) {
+      return this;
+    } else {
+      let parcels = this.parcels
+        .map(p => {
+          if (p.place != this.place) return p;
+          return { place: destination, address: p.address };
+        })
+        .filter(p => p.place != p.address);
+      return new VillageState(destination, parcels);
+    }
+  }
+}
+
+let first = new VillageState("Post Office", [
+  { place: "Post Office", address: "Alice's House" }
+]);
+let next = first.move("Alice's House");
+
+console.log(next.place);
+// → Alice's House
+console.log(next.parcels);
+// → []
+console.log(first.place);
+// → Post Office
