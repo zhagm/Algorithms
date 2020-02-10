2c3601db9b698a6161e6b36d663e881565bd540a
zhagm <zhagdev@gmail.com>
2020-02-09 18:37:40 -0800

    Add binary tree test

--- Leetcode/0_binary_tree_max_depth.js
+++ Leetcode/0_binary_tree_max_depth.js
@@ -1,16 +1,19 @@
-/**
- * Definition for a binary tree node.
- * function TreeNode(val) {
- *     this.val = val;
- *     this.left = this.right = null;
- * }
- */
-/**
- * @param {TreeNode} root
- * @return {number}
- */
+function TreeNode(val, left, right) {
+  this.val = val;
+  this.left = left || null;
+  this.right = right || null;
+}
 
 let maxDepth = function(root) {
   if (!root) return 0;
   return 1 + Math.max(maxDepth(root.left), maxDepth(root.right));
 };
+
+let head = new TreeNode(
+  3,
+  new TreeNode(9),
+  new TreeNode(20, new TreeNode(15), new TreeNode(7))
+);
+console.log(
+  `maxDepth([3,9,20,null,null,15,7]) => ${maxDepth(head)} => 3`
+);
