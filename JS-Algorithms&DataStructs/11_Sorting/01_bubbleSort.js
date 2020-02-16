0f2f92fa4d779c1b9608e9982342470f2cbd4d5a
zhagm <zhagdev@gmail.com>
2020-02-16 12:57:52 -0800

    Pseudocode for sorting pt.1

--- JS-Algorithms&DataStructs/11_Sorting/01_bubbleSort.js
+++ JS-Algorithms&DataStructs/11_Sorting/01_bubbleSort.js
@@ -1,14 +1,19 @@
 // Instructions:
 // ...
 
-function func(x) {
-  return x;
+function bubbleSort(x) {
+  return;
 }
 
-console.log("func('test'):", func('test'));
-// answer
+console.log("bubbleSort([5, 4, 3, 2, 1]):", bubbleSort([5, 4, 3, 2, 1]));
+console.log("bubbleSort([5, 3, 1, 2, 4]):", bubbleSort([5, 3, 1, 2, 4]));
+console.log("bubbleSort([1, 2, 3, 4, 5]):", bubbleSort([1, 2, 3, 4, 5]));
+// [1, 2, 3, 4, 5]
 
 /*
 Pseudocode:
-//
+* set counter at array length
+* loop through, while (counter is not equal to array length)
+* everytime a swap occurs, decrement counter
+* compare index 0 and index 1, swap if 0 is bigger
 */
