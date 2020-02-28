9d62c254ed0cdd10bfa3e236fdea6b227d5f1b1b
zhagm <zhagdev@gmail.com>
2020-02-28 10:13:51 -0500

    Complete insertion sort

--- JS-Algorithms&DataStructs/11_Sorting/03_insertionSort.js
+++ JS-Algorithms&DataStructs/11_Sorting/03_insertionSort.js
@@ -1,4 +1,11 @@
 function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] < arr[i - 1]) {
+      for (let j = i - 1; j >= 0; j--) {
+        if (arr[j + 1] < arr[j]) [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
+      }
+    }
+  }
   return arr;
 }
 
