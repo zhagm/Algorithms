6ae81621e21939bddb2251b9209450929a36b05d
zhagm <zhagdev@gmail.com>
2020-02-29 12:10:56 -0500

    Optimize insertion sort

--- JS-Algorithms&DataStructs/11_Sorting/03_insertionSort.js
+++ JS-Algorithms&DataStructs/11_Sorting/03_insertionSort.js
@@ -1,9 +1,7 @@
 function insertionSort(arr) {
   for (let i = 1; i < arr.length; i++) {
-    if (arr[i] < arr[i - 1]) {
-      for (let j = i - 1; j >= 0; j--) {
-        if (arr[j + 1] < arr[j]) [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
-      }
+    for (let j = i - 1; j >= 0, arr[j + 1] < arr[j]; j--) {
+      [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]];
     }
   }
   return arr;
