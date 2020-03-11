b10cc8d5d0b0140c097c15e2fb921f7bb744c0b7
zhagm <zhagdev@gmail.com>
2020-03-10 21:53:46 -0700

    Init interview cake, init reverseWords instructions

---
+++ Interview-Cake/01_Array_And_String_Manipulation/02_reverseWords.js
@@ -0,0 +1,14 @@
+/**
+ * Your team is scrambling to decipher a recent message, worried it's a plot to break into a major European National Cake Vault. 
+ * The message has been mostly deciphered, but all the words are backward! Your colleagues have handed off the last step to you.
+ * Write a function reverse_words() that takes a message as a list of characters and reverses the order of the words in place.
+ * Example:
+ const msg =   [ 'c', 'a', 'k', 'e', ' ',
+ 'p', 'o', 'u', 'n', 'd', ' ',
+ 's', 't', 'e', 'a', 'l' ];
+ 
+ reverseWords(msg);
+ 
+ console.log(message.join(''));
+ * When writing your function, assume the message contains only letters and spaces, and all words are separated by one space.
+**/
