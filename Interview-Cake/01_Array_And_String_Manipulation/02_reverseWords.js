5953804ba7e53a7310a8ccab0a5d22bbb09fe37d
zhagm <zhagdev@gmail.com>
2020-03-10 21:58:58 -0700

    Add test cases for 01/02

--- Interview-Cake/01_Array_And_String_Manipulation/02_reverseWords.js
+++ Interview-Cake/01_Array_And_String_Manipulation/02_reverseWords.js
@@ -2,13 +2,18 @@
  * Your team is scrambling to decipher a recent message, worried it's a plot to break into a major European National Cake Vault. 
  * The message has been mostly deciphered, but all the words are backward! Your colleagues have handed off the last step to you.
  * Write a function reverse_words() that takes a message as a list of characters and reverses the order of the words in place.
- * Example:
- const msg =   [ 'c', 'a', 'k', 'e', ' ',
- 'p', 'o', 'u', 'n', 'd', ' ',
- 's', 't', 'e', 'a', 'l' ];
- 
- reverseWords(msg);
- 
- console.log(message.join(''));
  * When writing your function, assume the message contains only letters and spaces, and all words are separated by one space.
 **/
+
+function reverseWords(arr) {
+
+}
+
+const test1 = [ 'c', 'a', 'k', 'e', ' ', 'p', 'o', 'u', 'n', 'd', ' ', 's', 't', 'e', 'a', 'l' ];
+reverseWords(test1);
+const test2 = [ 'b', 'e','a','c','h',' ','s','u','n','n','y',' ','t','h','e',' ','a','t' ];
+reverseWords(test2);
+ 
+
+console.log(`'steal pound cake' => ${test1.join('')}`);
+console.log(`'at the sunny beach' => ${test2.join('')}`);
